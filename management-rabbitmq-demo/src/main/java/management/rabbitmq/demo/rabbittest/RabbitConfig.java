20d87c9f4400df37dd29fa498657d2b063218ea8
xiqiuwei <571243494@qq.com>
2019-08-05 15:04:17 +0800

    RabbitMQ的核心配置类

--- management-rabbitmq-demo/src/main/java/management/rabbitmq/demo/rabbittest/RabbitConfig.java
+++ management-rabbitmq-demo/src/main/java/management/rabbitmq/demo/rabbittest/RabbitConfig.java
@@ -248,31 +248,7 @@ public class RabbitConfig {
         RetryPolicy retryPolicy =
                 new SimpleRetryPolicy(2, Collections.<Class<? extends Throwable>, Boolean>singletonMap(Exception.class, true));
         retryTemplate.setRetryPolicy(retryPolicy);
-        // 重试次数内会走这个方法
-        RetryCallback<Object, Exception> retryCallback = new RetryCallback<Object, Exception>() {
-            @Override
-            public Object doWithRetry(RetryContext retryContext) throws Exception {
-                // TODO 这里可以写自己的逻辑业务
-                log.info("这是重试的次数:{}", retryContext);
-                // 可以给当前的context传递一些键值对信息
-                retryContext.setAttribute("error", "看三土打篮球像菜虚困");
-                throw new Exception("重试次数达到上限还是报错");
-            }
-        };
-        // 这个方法是重试次数达到了上限并且还抛异常的情况下才会走
-        RecoveryCallback<Object> recoveryCallback = new RecoveryCallback<Object>() {
-            @Override
-            public Object recover(RetryContext retryContext) throws Exception {
-                // TODO 这里可以写自己的逻辑业务
-                log.info("这是报错信息:{}", retryContext.getLastThrowable().getMessage());
-                return null;
-            }
-        };
-        try {
-            retryTemplate.execute(retryCallback,recoveryCallback);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
         return retryTemplate;
     }
 }
